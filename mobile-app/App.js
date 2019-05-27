b4dd3bf003301c0d28349ebb51f15ed0dec78bc1
David <eth3rnit3@outlook.fr>
2019-05-27 10:58:48 +0200

    fix app of mobile app

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import store from './store'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+import store from './store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import persistor from './store/persistor';
 import StackNavigator from './navigation/StackNavigator';
 
