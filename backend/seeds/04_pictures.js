51d6907f2658d10562dff4ffdb98b2a3e5198e60
David <eth3rnit3@outlook.fr>
2019-06-04 14:30:04 +0200

    fix seed data

---
+++ backend/seeds/04_pictures.js
@@ -0,0 +1,122 @@
+const models = require('../models');
+const Picture = models.Picture;
+const faker = require('faker');
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 1
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 1
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 3
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 3
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 3
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 4
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 4
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 5
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 5
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 5
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 6
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 6
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 7
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 7
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 7
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 8
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
+
+Picture.create({
+  url: faker.image.imageUrl(),
+  maraudeId: 8
+})
+  .then((maraude) => { console.log(maraude); })
+  .catch((error) => { console.log(error); });
--- backend/seeds/test.js
+++ backend/seeds/test.js
@@ -1,68 +1,6 @@
 const models = require('../models');
 const User = models.User;
-const Picture = models.Picture;
 const Maraude = models.Maraude;
-
-// Create User (godFather)
-User.create({
-  firstName: 'Parrain',
-  lastName: 'DELEPAMPA',
-  email: 'taattatata@gmail.com',
-  password: 'test1234',
-  avatarUrl: 'www.google/image1.png',
-  isAdmin: false,
-  isActive: true,
-  isBanned: false,
-  invitationCode: 'abcd',
-  job: 'coiffeur'
-})
-  .then((user) => { console.log(user); })
-  .catch((error) => { console.log(error); });
-
-// Create other User (childFather)
-User.create({
-  firstName: 'Filleul',
-  lastName: 'DOE',
-  email: 'filleul@gmail.com',
-  password: 'test1234',
-  avatarUrl: 'www.google/image2.png',
-  isAdmin: false,
-  isActive: true,
-  isBanned: false,
-  invitationCode: 'efgh',
-  job: 'coiffeur'
-})
-  .then((user) => { console.log(user); })
-  .catch((error) => { console.log(error); });
-
-// Create Maraude and associated pictures
-Maraude.create({
-  userId: 1,
-  title: 'Maraude no 1',
-  startAt: new Date(),
-  endAt: new Date(),
-  description: 'la description',
-  city: 'Marseille',
-  isPublished: true,
-  longitude: '54.4783783748',
-  latitude: '6.783748',
-  photos: [
-    { url: 'www.google.fr/image8.png' },
-    { url: 'www.google.fr/image9.png' },
-    { url: 'www.google.fr/image10.png' },
-  ]
-}, { include: ['photos'] })
-  .then((maraude) => { console.log(maraude); })
-  .catch((error) => { console.log(error); });
-
-// Create pictures without associations
-Picture.bulkCreate([
-  { url: 'www.google.fr/image1.png' },
-  { url: 'www.google.fr/image2.png' },
-  { url: 'www.google.fr/image3.png' },
-])
-  .then((pictures) => { console.log(pictures); })
-  .catch((error) => { console.log(error); });
 
 // Find Maraude by id
 Maraude.findByPk(1, { include: ['photos', 'author'] })
