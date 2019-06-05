02f2fa8dd6745ff379cf1ec2e28b131bdd5a4297
Djamel <djamel.ammour@gmail.com>
2019-06-05 23:03:02 +0200

    Structure tabs navigation footer

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import store from './store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import persistor from './store/persistor';
-import StackNavigator from './navigation/StackNavigator';
-import { Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import { Text } from 'react-native'
+import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistor from "./store/persistor";
+import StackNavigator from "./navigation/StackNavigator";
+import { Font } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import { Text } from "native-base";
+import { SafeAreaView } from "react-navigation";
+
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       loading: true
-    }
+    };
   }
 
   async componentDidMount() {
     await Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
     });
-    this.setState({ loading: false })
-  };
+    this.setState({ loading: false });
+  }
 
   render() {
     if (this.state.loading) {
-      return <Text>Loading ...</Text>
+      return <Text>Loading ...</Text>;
     }
     return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaView
+            style={{ backgroundColor: "#F6F6F6" }}
+            forceInset={{ bottom: "never" }}
+          />
           <StackNavigator />
         </PersistGate>
       </Provider>
