24245d9333a4e28fbebbb388a7e508ab0182793d
Djamel <djamel.ammour@gmail.com>
2019-06-07 12:18:48 +0200

    Add Map Maraudes

--- mobile-app/navigation/StackNavigator.js
+++ mobile-app/navigation/StackNavigator.js
@@ -7,43 +7,6 @@ import BottomTabNav from './bottomTabNavigator'
 import LoginForm from "../screens/public/LoginForm";
 import Profile from "../screens/connected/Profile";
 
-const TabNavFooter = createBottomTabNavigator({
-    Map: {
-      screen: MapMaraudes,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor }) => <Icon name="ios-pin" size={30} color={tintColor} />
-      },  
-    },
-    List: {
-      screen: ListMaraudes,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor }) => <Icon name="ios-calendar" size={30} color={tintColor} />
-      },  
-    },
-    Feed: {
-      screen: FeedMaraudes,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor }) => <Icon name="ios-images" size={30} color={tintColor} />
-      },  
-    },
-    Menu: {
-      screen: Menu,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor }) => <Icon name="ios-more" size={30} color={tintColor} />
-      },  
-    }
-  },
-  {
-    initialRouteName: "Map",
-    tabBarOptions: {
-      activeBackgroundColor: "#2D2D2D",
-      inactiveBackgroundColor: "#2D2D2D",
-      activeTintColor: "#FDC500",
-      showLabel: false
-    },
-  },
-);
-
 const AppStack = createStackNavigator(
   {
     Profile: { screen: Profile }
