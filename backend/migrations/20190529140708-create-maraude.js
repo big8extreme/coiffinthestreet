780f6d4113b14ab7eb7c219ba080af3f25ad1656
hafid <cyberhafid@gmail.com>
2019-06-07 16:06:09 +0200

    maj

--- backend/migrations/20190529140708-create-maraude.js
+++ backend/migrations/20190529140708-create-maraude.js
@@ -13,8 +13,8 @@ module.exports = {
         allowNull: false,
         references: {
           model: 'Users',
-         key: 'id'
-       }
+          key: 'id'
+        }
       },
       title: {
         type: Sequelize.STRING
