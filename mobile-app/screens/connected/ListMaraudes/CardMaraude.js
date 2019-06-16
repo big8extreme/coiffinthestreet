479a416a6ecf090ec2235e94d52c8bcd41cb8cde
Djamel <djamel.ammour@gmail.com>
2019-06-16 11:47:56 +0200

    Add key list maraudes

--- mobile-app/screens/connected/ListMaraudes/CardMaraude.js
+++ mobile-app/screens/connected/ListMaraudes/CardMaraude.js
@@ -29,9 +29,9 @@ export default class CardMaraude extends Component {
     render() {
         return (
           <ScrollView style={{ backgroundColor: "#FFF" }}>
-          {this.state.maraudes.map(maraude => {
+          {this.state.maraudes.map((maraude, index) => {
             return (
-              <View style={styles.shadow}>
+              <View key={index} style={styles.shadow}>
                 <Icon
                   style={{
                     color: "#E6E6E6",
@@ -106,7 +106,7 @@ export default class CardMaraude extends Component {
                     d="M43.2791592,3.5 C40.0544477,3.5 37.1074306,5.32478241 35.6701474,8.21147275 L11.5481664,56.6589108 C10.9922563,57.7754201 10.6888692,59.0005889 10.6595302,60.2474924 C10.5491032,64.9406138 14.2641111,68.8346591 18.9572325,68.945086 L287.216538,75.2571015 C291.432985,75.3563125 295.084493,72.3487197 295.795003,68.1913844 L305.153668,13.4319309 C305.234511,12.9589056 305.27515,12.4798838 305.27515,12 C305.27515,7.30557963 301.46957,3.5 296.77515,3.5 L43.2791592,3.5 Z"
                     fill="#112249"
                   />
-                <Text style={{ color: '#FFF', paddingLeft: 90, position: 'absolute', paddingTop: 40 }}>Je souhaite participer</Text>
+                {/* <Text style={{ color: '#FFF', paddingLeft: 90, position: 'absolute', paddingTop: 40 }}>Je souhaite participer</Text> */}
                 </Svg>
                 </View>
               </View>
