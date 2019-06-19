2bc5a6ccf1f28d0932b45a643094abf101fab8fb
David <eth3rnit3@outlook.fr>
2019-06-19 11:29:43 +0200

    reset redux

--- backoffice/src/App.jsx
+++ backoffice/src/App.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Router from './routes/Router';
-import {Provider} from 'react-redux';
-import store from './stores';
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store ={store} >
-    <Router />
-    </Provider>
-    )
+      <Router />
+    );
   }
 }
