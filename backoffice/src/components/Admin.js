b646d2c10c3f9e9f13adc9783ca46bc9b18e67e0
cyberhafid <cyberhafid@gmai.com>
2019-06-24 22:54:51 +0200

    update switch

--- backoffice/src/components/Admin.js
+++ backoffice/src/components/Admin.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../stores/actions/auth';
-import MenuLeft from './contents/menuleft';
+import MenuLeft from './contents/Menuleft';
 import AdminRouter from '../routes/AdminRouter';
 
 export class Admin extends Component {
