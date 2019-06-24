90c482bd4a71f428d58dfb46bcd9f07e06589f9f
Urth <dragondor@protonmail.com>
2019-06-24 15:29:06 +0200

    fetch and show participants created in store

--- mobile-app/store/actions/participant.js
+++ mobile-app/store/actions/participant.js
@@ -1,7 +1,47 @@
 import axios from 'axios';
 import { baseUrlApi } from '../../apiUrl'
 
-import { CREATE_PARTICIPANT, ERROR_ON_CREATE_PARTICIPANT } from '../types/participant'
+import { FETCH_PARTICIPANTS, ERROR_ON_PARTICIPANTS, FETCH_PARTICIPANT, ERROR_ON_PARTICIPANT, CREATE_PARTICIPANT, ERROR_ON_CREATE_PARTICIPANT } from '../types/participant'
+
+export function fetchParticipants(){
+    return async function(dispatch, getState) {
+        function onSuccess(response){
+            dispatch({ type: FETCH_PARTICIPANTS, payload: response.data.participants})
+        }
+        function onError(error){
+            dispatch({ type: ERROR_ON_PARTICIPANTS, payload: error})
+        }
+        try{
+            const response = await axios.post(`${baseUrlApi}/participants/`, { ...participantFields }, {
+                headers: { Authorization: `bearer ${getState().auth.user.token }` }
+            })
+            onSuccess(response)
+        }
+        catch(err){
+            onError(err)
+        }
+    }
+}
+
+export function showParticipant(participantId){
+    return async function(dispatch, getState) {
+        function onSuccess(response){
+            dispatch({ type: FETCH_PARTICIPANT, payload: response.data.participants})
+        }
+        function onError(error){
+            dispatch({ type: ERROR_ON_PARTICIPANT, payload: error})
+        }
+        try{
+            const response = await axios.post(`${baseUrlApi}/participants/${ participantId }`, {
+                headers: { Authorization: `bearer ${getState().auth.user.token }` }
+            })
+            onSuccess(response)
+        }
+        catch(err){
+            onError(err)
+        }
+    }
+}
 
 export function createParticipant(participantFields){
     return async function(dispatch, getState) {
--- mobile-app/store/reducers/participant.js
+++ mobile-app/store/reducers/participant.js
@@ -1,7 +1,7 @@
 import {
-
-    CREATE_PARTICIPANT,
-    ERROR_ON_CREATE_PARTICIPANT,
+    FETCH_PARTICIPANTS, ERROR_ON_PARTICIPANTS,
+    FETCH_PARTICIPANT, ERROR_ON_PARTICIPANT,
+    CREATE_PARTICIPANT, ERROR_ON_CREATE_PARTICIPANT,
 
 } from "./../types/participant";
 
@@ -10,6 +10,14 @@ const defaultStates = {
 };
 export default function (state = defaultStates, action) {
     switch (action.type) {
+        case FETCH_PARTICIPANTS:
+            return { ...state, participants: [...action.payload] };
+        case ERROR_ON_PARTICIPANTS:
+            return state;
+        case FETCH_PARTICIPANT:
+            return { ...state };
+        case ERROR_ON_PARTICIPANT:
+            return state;
         case CREATE_PARTICIPANT:
             return { ...state };
         case ERROR_ON_CREATE_PARTICIPANT:
