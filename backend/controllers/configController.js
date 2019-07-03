806a4542db8fd9f1f2af46e467741bb8e694fbe8
Radouane <Boukerche.r@laposte.net>
2019-07-03 17:14:52 +0200

    configure send mail

--- backend/controllers/configController.js
+++ backend/controllers/configController.js
@@ -1,5 +1,6 @@
 const models = require('../models');
 const Config = models.Config;
+const mailer = require('../mailer/mailer')
 
 module.exports = {
     index: function (req, res, next) {
@@ -24,4 +25,27 @@ module.exports = {
             })
             .catch((error) => res.status(500).json({ error }));
     },
+
+    update: function (req, res, next) {
+        const user = {
+            id: req.body.id,
+        };
+    },
+
+    contactAdmin: function (req, res, next) {
+        // const message = {
+        //     subject: req.body.subject,
+        //     message: req.body.message
+        // }
+        const userDatas = {
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            subject: req.body.subject,
+            message: req.body.message
+        };
+        mailer(userDatas, userDatas.email, 'contactAdmin')
+        res.json({ message: userDatas });
+    }
+
 };
--- backend/controllers/participantsController.js
+++ backend/controllers/participantsController.js
@@ -1,5 +1,8 @@
 const models = require('../models');
 const Participant = models.Participant;
+const mailer = require('../mailer/mailer');
+const Maraude = require('../models/maraude');
+const User = require('../models/user')
 
 module.exports = {
     index: function (req, res, next) {
@@ -23,33 +26,63 @@ module.exports = {
             city: req.body.city,
             age: req.body.age
         })
-            .then((participant) => { res.json({ participant }); })
-            .catch((error) => res.status(500).json({ error }));
-    },
-    update: function (req, res, next) {
-        Participant.findByPk(req.params.id)
             .then((participant) => {
-                participant.update({
-                    maraudeId: req.body.maraudeId,
-                    isValidate: req.body.isValidate,
-                    email: req.body.email,
-                    job: req.body.job,
-                    lastName: req.body.lastName,
-                    firstName: req.body.firstName,
-                    city: req.body.city,
-                    age: req.body.age
+                const userDatas = {
+                    firstName: participant.firstName,
+                    lastName: participant.lastName,
+                }
+                Maraude.findAll({
+                    where: {
+                        maraudeId: req.body.maraudeId //Todo add email as query parameter
+                    }
+                        .then((coiffeur) => {
+                            Maraude.findOne({
+                                where: {
+                                    userId: req.body.userId,
+                                }
+                                    .then((user) => {
+                                        User.findAll({
+                                            where: {
+                                                userId: userId
+                                            }
+                                                .then((user) => {
+                                                    const recipient = {
+                                                        email: email.email,
+                                                    }
+                                                })
+                                        })
+                                    })
+                            })
+                        })
+                    })
+                    .catch((error) => res.status(500).json({ error }));
+                    res.json({ participant });
                 })
-                    .then((updatedParticipant) => { res.json({ participant: updatedParticipant }); })
-                    .catch((error) => res.status(500).json({ error }));
-            });
-    },
-    delete: function (req, res, next) {
-        Participant.findByPk(req.params.id)
-            .then((participant) => {
-                participant.destroy()
-                    .then((participant) => { res.json({ message: 'Participant has been deleted !' }); })
-                    .catch((error) => res.status(500).json({ error }));
-            })
-            .catch((error) => res.status(500).json({ error }));
-    },
+        },
+                update: function (req, res, next) {
+                    Participant.findByPk(req.params.id)
+                        .then((participant) => {
+                            participant.update({
+                                maraudeId: req.body.maraudeId,
+                                isValidate: req.body.isValidate,
+                                email: req.body.email,
+                                job: req.body.job,
+                                lastName: req.body.lastName,
+                                firstName: req.body.firstName,
+                                city: req.body.city,
+                                age: req.body.age
+                            })
+                                .then((updatedParticipant) => { res.json({ participant: updatedParticipant }); })
+                                .catch((error) => res.status(500).json({ error }));
+                        });
+                },
+                delete: function (req, res, next) {
+                    Participant.findByPk(req.params.id)
+                        .then((participant) => {
+                            participant.destroy()
+                                .then((participant) => { res.json({ message: 'Participant has been deleted !' }); })
+                                .catch((error) => res.status(500).json({ error }));
+                        })
+                        .catch((error) => res.status(500).json({ error }));
+                },
 };
