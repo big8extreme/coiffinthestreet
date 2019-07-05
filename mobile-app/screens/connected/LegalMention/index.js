ef9b6d78432178b11b56e4043ae7b736cd85bcd9
Miatack <miatack529@gmail.com>
2019-07-05 16:37:12 +0200

    Fix stack navigator , merge screen/CGU and update gitignore

--- mobile-app/screens/connected/LegalMention/index.js
+++ mobile-app/screens/connected/LegalMention/index.js
@@ -7,67 +7,67 @@ import { connect } from 'react-redux'
 import { fetchConfigs } from '../../../store/actions/config'
 
 export class LegalMentions extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            keyWord: null
-        };
-    }
-    componentDidMount() {
-        this.props.fetchConfigs();
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyWord: null
+    };
+  }
+  componentDidMount() {
+    this.props.fetchConfigs();
 
-    }
+  }
 
-    // static propTypes = {
-    //     prop: PropTypes
-    // }
+  // static propTypes = {
+  //     prop: PropTypes
+  // }
 
-    render() {
-        return (
-            <Provider store={store}>
-                <View style={styles.backgroundApp}>
-                    <Text style={styles.Titletext}>MENTIONS LEGALES</Text>
-                    <ScrollView>
-                      <Text style={styles.textLegal}>{this.props.config.legalMention}</Text>
-                    </ScrollView>
-                </View>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={styles.backgroundApp}>
+          <Text style={styles.Titletext}>MENTIONS LEGALES</Text>
+          <ScrollView>
+            <Text style={styles.textLegal}>{this.props.config.legalMention}</Text>
+          </ScrollView>
+        </View>
+      </Provider>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-    config: state.config.currentConfig
+  config: state.config.currentConfig
 })
 
 const mapDispatchToProps = {
-    fetchConfigs
+  fetchConfigs
 
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LegalMentions);
 
 const styles = StyleSheet.create({
-    backgroundApp: {
-        backgroundColor: '#4E4E4E', flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    Titletext: {
-        color: 'white',
-        fontSize: 35,
-        marginTop: 20,
-        fontFamily: "Sedgwick",
-        justifyContent: 'center',
-    },
-    textLegal: {
-        color: 'white',
-        fontSize: 15,
-        marginRight: 15,
-        marginLeft: 15,
-        marginBottom: 15,
-        marginTop: 15,
-        textAlign: 'center',
-    },
+  backgroundApp: {
+    backgroundColor: '#4E4E4E', flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Titletext: {
+    color: 'white',
+    fontSize: 35,
+    marginTop: 20,
+    fontFamily: "Sedgwick",
+    justifyContent: 'center',
+  },
+  textLegal: {
+    color: 'white',
+    fontSize: 15,
+    marginRight: 15,
+    marginLeft: 15,
+    marginBottom: 15,
+    marginTop: 15,
+    textAlign: 'center',
+  },
 });
