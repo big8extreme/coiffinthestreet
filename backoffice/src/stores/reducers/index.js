98ef281f3a2ee44c868799516e16e60be9be9154
David <eth3rnit3@outlook.fr>
2019-07-09 10:45:56 +0200

    add message notice and alert

--- backoffice/src/stores/reducers/index.js
+++ backoffice/src/stores/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import userReducer from './user';
 import authReducer from './authReducer';
-import configReducer from './config'
+import configReducer from './config';
+import appReducer from './app';
 
 
 export default combineReducers({
   authentification: authReducer,
   user: userReducer,
-  config: configReducer
+  config: configReducer,
+  app: appReducer,
 });
 
