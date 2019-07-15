9a8e75202d65198dffe1e3bea615dcec72062ea1
Bastien Alessandri <bastienalessandri@gmail.com>
2019-07-15 17:32:37 +0200

    add GlobalFooter on Chart screen (and change some css)

--- mobile-app/screens/public/Charte/index.js
+++ mobile-app/screens/public/Charte/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import { connect } from 'react-redux'
 import { fetchConfigs } from '../../../store/actions/config'
+import GlobalFooter from '../../../components/GlobalFooter';
 
 export class Charte extends Component {
   constructor(props) {
@@ -15,9 +16,12 @@ export class Charte extends Component {
   }
   render() {
     return (
-      <View style={styles.backgroundApp}>
-        <Text style={styles.Titletext}>Charte d'utilisateur</Text>
-        <Text style={styles.textCharte}>{this.props.config.charte}</Text>
+      <View style={{flex: 1, backgroundColor: '#2D2D2D'}} >
+        <ScrollView>
+            <Text style={styles.Titletext}>Charte d'utilisateur</Text>
+            <Text style={styles.textCharte}>{this.props.config.charte}</Text>
+        </ScrollView>
+        <GlobalFooter/>
       </View>
     );
   }
@@ -34,16 +38,11 @@ const mapDispatchToProps = {
 export default connect(mapStateToProps, mapDispatchToProps)(Charte);
 
 const styles = StyleSheet.create({
-  backgroundApp: {
-    backgroundColor: '#2D2D2D', flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   Titletext: {
     color: 'white',
     fontSize: 35,
     fontFamily: "Sedgwick",
+    alignSelf: 'center',
   },
   textCharte: {
     color: 'white',
