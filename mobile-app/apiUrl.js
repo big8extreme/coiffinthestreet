6bdf00b83f14557bace371bfcf09c57f1d32fa2e
Dieudonné <tchoule84@users.noreply.github.com>
2019-07-19 19:24:11 +0200

    Update apiUrl.js
    Uncomment file

--- mobile-app/apiUrl.js
+++ mobile-app/apiUrl.js
@@ -1,9 +1,7 @@
-// export const baseUrl = __DEV__
-//   ?
-//   'http://192.168.0.21:5000'
-//   :
-//   'https://api.coiffinthestreet.com';
-
-  export const baseUrl =   'http://192.168.0.21:5000'
+export const baseUrl = __DEV__
+  ?
+  'http://192.168.1.63:5000'
+  :
+  'https://api.coiffinthestreet.com';
   
 export const baseUrlApi = `${baseUrl}/api/v1`;
