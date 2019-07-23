49e23bcbfdd2b66939ba6620df78192c647f967f
Dieudonné <tchoule84@users.noreply.github.com>
2019-07-23 17:33:02 +0200

    Update 03_participants.js
    Update Participants Seed

--- backend/seeds/03_participants.js
+++ backend/seeds/03_participants.js
@@ -1,17 +1,104 @@
 const models = require('../models');
 const Participant = models.Participant;
-const faker = require('faker');
 
-const activities = ['coiffeur', 'estheticien', 'photographe'];
 
-for(let i = 1; i < 11; i++){
-  
-  Participant.create({
-    maraudeId: 2,
-    isValidate: true,
-    email: faker.internet.email(),
-    job: activities[Math.floor(Math.random()*activities.length)]
+// const activities = ['coiffeur', 'estheticien', 'photographe'];
+
+Participant.create({
+  maraudeId: 2,
+  isValidate: true,
+  email: 'rol@gto.li',
+  job: 'Coiffeur'
+})
+  .then((participant) => { console.log(participant); })
+  .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 4,
+  isValidate: true,
+  email: 'moi@toi.fr',
+  job: 'Esthéticien(ne)'
+})
+  .then((participant) => { console.log(participant); })
+  .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 5,
+  isValidate: true,
+  email: 'gir@hut.li',
+  job: 'Coiffeur'
+})
+    .then((participant) => { console.log(participant); })
+    .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 12,
+  isValidate: true,
+  email: 'frr@fee.fr',
+  job: 'Photographe'
   })
     .then((participant) => { console.log(participant); })
     .catch((error) => { console.log(error); });
-}
+
+Participant.create({
+  maraudeId: 2,
+  isValidate: true,
+  email: 'rou@filter.uk',
+  job: 'Esthéticien(ne)'
+})
+    .then((participant) => { console.log(participant); })
+    .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 3,
+  isValidate: true,
+  email: 'gra@hut.fil',
+  job: 'Coiffeur'
+})
+    .then((participant) => { console.log(participant); })
+    .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 9,
+  isValidate: true,
+  email: 'yolo@tg.fr',
+  job: 'Photographe'
+})
+    .then((participant) => { console.log(participant); })
+    .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 3,
+  isValidate: true,
+  email: 'koa@la.gb',
+  job: 'Coiffeur'
+})
+    .then((participant) => { console.log(participant); })
+    .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 3,
+  isValidate: true,
+  email: 'licorn@tu.us',
+  job: 'Coiffeur'
+})
+    .then((participant) => { console.log(participant); })
+    .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 1,
+  isValidate: true,
+  email: 'git@lafon.gr',
+  job: 'Esthéticien(ne)'
+})
+    .then((participant) => { console.log(participant); })
+    .catch((error) => { console.log(error); });
+
+Participant.create({
+  maraudeId: 5,
+  isValidate: true,
+  email: 'thus@koaloa.ch',
+  job: 'Photographe'
+})
+    .then((participant) => { console.log(participant); })
+    .catch((error) => { console.log(error); });
