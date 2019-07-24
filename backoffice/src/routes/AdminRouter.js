da72f8d2ff279ba95e2a60a98e8384a2159763e8
David <eth3rnit3@outlook.fr>
2019-07-24 17:25:37 +0200

    add doc link

--- backoffice/src/routes/AdminRouter.js
+++ backoffice/src/routes/AdminRouter.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import Dashboard from '../pages/Dashboard';
 import UserPanel from '../pages/UserPanel';
 import ConfigPage from '../pages/ConfigPage';
-import DocApi from '../pages/DocApi';
 import MaraudesPanel from '../pages/MaraudesPanel';
 class AdminRouter extends Component {
   render() {
@@ -16,7 +15,6 @@ class AdminRouter extends Component {
         <Route exact path='/admin/users' component={UserPanel} />
         <Route exact path='/admin/maraudes' component={MaraudesPanel} />
         <Route exact path='/admin/configs' component={ConfigPage} />
-        <Route exact path='/admin/docapi' component={DocApi} />
       </Switch>
     );
   }
