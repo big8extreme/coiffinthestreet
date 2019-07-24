d2f9b30d47747e79dad5ec313dd46891b5f5b72d
David <eth3rnit3@outlook.fr>
2019-07-24 18:27:14 +0200

    fix search by city

--- mobile-app/screens/connected/ListMaraudes/CardsMaraudes.js
+++ mobile-app/screens/connected/ListMaraudes/CardsMaraudes.js
@@ -8,18 +8,18 @@ class CardsMaraudes extends Component {
   render() {
     return (
       <ScrollView style={{ backgroundColor: "#FBFBFB" }}>
-      {
-        this.props.maraudes.length>1 ? 
-        this.props.maraudes.map((maraude, index) => {
-            return (
-            <View key={index}>
-              <CardMaraude currentUserId={this.props.user.id} maraude={maraude} navigation={this.props.navigation} />
-            </View>
-          );
-        })
-        :
-        <Text style={{ fontStyle: 'italic', padding: 10 }}>Désolé il n'y a pas de maraude dans cette ville</Text>
-      }
+        {
+          this.props.maraudes.length >= 1 ?
+            this.props.maraudes.map((maraude, index) => {
+              return (
+                <View key={index}>
+                  <CardMaraude currentUserId={this.props.user.id} maraude={maraude} navigation={this.props.navigation} />
+                </View>
+              );
+            })
+            :
+            <Text style={{ fontStyle: 'italic', padding: 10 }}>Désolé il n'y a pas de maraude dans cette ville</Text>
+        }
       </ScrollView>
     );
   }
